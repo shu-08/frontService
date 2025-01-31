8f6a68f8237dcef5860d280c0f344d74ca89a696
shu-08 <shu.maruyama08@gmail.com>
2025-02-01 00:42:27 +0900

    Create Dash.js

---
+++ src/Dash.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import DownloadingIcon from '@mui/icons-material/Downloading';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { useDemoRouter } from '@toolpad/core/internal';
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import VideoDownload from './TikTok/video/VideoDownload.js';
+
+const NAVIGATION = [
+  {
+    segment: 'TikTokVideo',
+    title: '動画保存',
+    icon: <DownloadForOfflineIcon />,
+  },
+  {
+    segment: 'TikTokeImage',
+    title: '画像保存',
+    icon: <DownloadingIcon />,
+  },
+];
+
+const demoTheme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: 'data-toolpad-color-scheme',
+  },
+  colorSchemes: { light: true, dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function DemoPageContent({ pathname }) {
+    return (
+      <Box
+        sx={{
+          py: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}
+      >
+        {pathname === '/TikTokVideo' ? (
+          <Typography variant="h5" color="primary">
+            <VideoDownload />
+          </Typography>
+        ) : (
+          <Typography>Dashboard content for {pathname}</Typography>
+        )}
+      </Box>
+    );
+  }
+
+DemoPageContent.propTypes = {
+  pathname: PropTypes.string.isRequired,
+};
+
+function DashboardLayoutBranding(props) {
+  const { window } = props;
+
+  const router = useDemoRouter('/dashboard');
+
+  // Remove this const when copying and pasting into your project.
+  const demoWindow = window !== undefined ? window() : undefined;
+
+  return (
+    // preview-start
+    <AppProvider
+      navigation={NAVIGATION}
+      branding={{
+        logo: <img src="https://mui.com/static/logo.png" alt="MUI logo" />,
+        title: 'TikTok Video Downloader',
+        homeUrl: '/toolpad/core/introduction',
+      }}
+      router={router}
+      theme={demoTheme}
+      window={demoWindow}
+    >
+      <DashboardLayout>
+        <DemoPageContent pathname={router.pathname} />
+      </DashboardLayout>
+    </AppProvider>
+    // preview-end
+  );
+}
+
+DashboardLayoutBranding.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * Remove this when copying and pasting into your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DashboardLayoutBranding;
